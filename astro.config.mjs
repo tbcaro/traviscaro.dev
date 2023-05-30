a406c3e2838f414216f4b978e18f864967b0d34c
Travis Caro <travis@carboncreek.agency>
2023-05-30 13:59:43 -0400

    Setup astro website basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify/functions";
+import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), mdx(), image()],
+  output: "server",
+  adapter: netlify()
+});
